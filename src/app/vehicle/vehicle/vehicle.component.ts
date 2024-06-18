d0d76e77556ac6557042a7020cead02d675b04ea
Vanessa Coin <57774408+vanessacoin@users.noreply.github.com>
2024-06-18 20:06:29 -0300

    feature: vehicle table html

--- src/app/vehicle/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle/vehicle.component.ts
@@ -1,19 +1,58 @@
-import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Observable } from 'rxjs';
+
+import { Vehicle } from '../model/vehicle';
+import { VehicleService } from '../services/vehicle.service';
 
 @Component({
   selector: 'app-vehicle',
   standalone: true,
-  imports: [MatTabsModule, MatInputModule, MatFormFieldModule, FormsModule, MatSelectModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatTabsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    HttpClientModule,
+    MatCardModule],
   templateUrl: './vehicle.component.html',
-  styleUrl: './vehicle.component.scss'
+  styleUrl: './vehicle.component.scss',
+  providers: [VehicleService]
 })
-export class VehicleComponent {
-  selectCustomerFormControl = new FormControl('', Validators.required);
+
+export class VehicleComponent implements OnInit {
+  @Input() vehicle_list: Vehicle[] = [];
+  @Output() add = new EventEmitter(false);
+  @Output() edit = new EventEmitter(false);
+  @Output() remove = new EventEmitter(false);
+
+  vehicles: Observable<Vehicle[]>;
+  displayedColumns = ['brand', 'model', 'plate', 'year', 'color', 'id_customer', 'actions']
+
+  constructor(private vehicleService: VehicleService) {
+    this.vehicles = this.vehicleService.list();
+   }
+
+  ngOnInit(): void { }
+
+  onAdd() {
+    this.add.emit(true);
+  }
+
+  onEdit(vehicle: Vehicle) {
+    this.edit.emit(vehicle);
+  }
+
+  onDelete(vehicle: Vehicle) {
+    this.remove.emit(vehicle);
+  }
 }
+
+
 
