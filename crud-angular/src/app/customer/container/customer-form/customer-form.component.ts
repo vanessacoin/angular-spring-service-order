f60d498468fe5c31ce59bb103a462b2e8965e9c3
Vanessa Coin <57774408+vanessacoin@users.noreply.github.com>
2024-09-18 15:37:09 -0300

    feat: created customer-form component to register and edit customer

--- crud-angular/src/app/customer/container/customer-form/customer-form.component.ts
+++ crud-angular/src/app/customer/container/customer-form/customer-form.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardActions, MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-customer-form',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatCardActions,
+    MatButtonModule],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.scss'
 })
-export class CustomerFormComponent {
+
+export class CustomerFormComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      cpf: [null],
+      phone: [null],
+      email: [null]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+
+  }
 
 }
--- crud-angular/src/app/customer/customer.module.ts
+++ crud-angular/src/app/customer/customer.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 
@@ -7,6 +8,7 @@ import { CustomerRoutingModule } from './customer-routing.module';
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
     CustomerRoutingModule
   ]
 })
